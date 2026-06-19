90c3344f202221ac6f4d97afb62a88c6e593336a
devkaransingh1 <2k24.csai1b.2413455@gmail.com>
2026-06-20 01:13:44 +0530

    going to deploye the flask app , db is already hosted on Render.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 db = SQLAlchemy()
 
@@ -8,10 +10,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'my_secret_key'
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-        "DATABASE_URL",
-        "postgresql://bluecred_user:1234@localhost/bluecred"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
