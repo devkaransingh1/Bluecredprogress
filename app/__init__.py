faee649e65e070921b0ab7ff6703e5433b0787d7
devkaransingh1 <2k24.csai1b.2413455@gmail.com>
2026-06-20 01:27:22 +0530

    mp last commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'my_secret_key'
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
